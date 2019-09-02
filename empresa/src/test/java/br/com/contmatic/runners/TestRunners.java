45f3374e43e0b96f10f18354e492b395ee95ff52
gegeuKaue <geovane.santos@contmatic.com.br>
2019-09-02 15:12:42 -0300

    2.0.0

--- empresa/src/test/java/br/com/contmatic/runners/TestRunners.java
+++ empresa/src/test/java/br/com/contmatic/runners/TestRunners.java
@@ -6,10 +6,11 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import br.com.contmatic.empresa.EmpresaTest;
 import br.com.contmatic.empresa.FuncionarioTest;
+import br.com.contmatic.endereco.EnderecoTest;
 import br.com.contmatic.telefone.TelefoneTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ EmpresaTest.class, FuncionarioTest.class, TelefoneTest.class })
+@SuiteClasses({ EmpresaTest.class, FuncionarioTest.class, TelefoneTest.class, EnderecoTest.class })
 public class TestRunners {
 
 }
